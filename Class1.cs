fa96ce37e7fc27226444cc0accb4a8066ae0299b
Teotia444 <comptest444@gmail.com>
2025-05-14 21:23:40 +0200

    updated to use a queue instead of being stupid

--- Class1.cs
+++ Class1.cs
@@ -3,6 +3,8 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace TCPGeckoAromaLibrary
 {
@@ -19,13 +21,52 @@ namespace TCPGeckoAromaLibrary
         public bool connected;
         public Socket client;
         public bool debug;
-        public List<string> busy = new List<string>();
+
+        public Queue<(string, string)> commands = new Queue<(string, string)>();
+        public Dictionary<string, string> results = new Dictionary<string, string>();
+        Thread commandHandler;
+
         
+
+        public void CommandHandler()
+        {
+            while (connected)
+            {
+                if (commands.Count == 0) continue;
+                var queueElement = commands.Dequeue();
+                var message = queueElement.Item1;
+
+                byte[] msg = Encoding.UTF8.GetBytes(message);
+                byte[] bytes = new byte[256];
+                string result = "";
+                try
+                {
+                    // Blocks until send returns.
+                    int byteCount = client.Send(msg, 0, msg.Length, SocketFlags.None);
+
+                    // Get reply from the server.
+                    byteCount = client.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+
+                    if (byteCount > 0)
+                    {
+                        result = Encoding.UTF8.GetString(bytes, 0, byteCount);
+                    }
+                    
+
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("{0} Error code: {1}.", e.Message, e.ErrorCode);
+                    continue;
+                }
+
+                results[queueElement.Item2] = result;
+            }
+        }
+
         public void Connect(string ipAddress, bool _debug = false)
         {
-            busy.Clear();
             debug = _debug;
-
             IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ipAddress), 7332);
             client = new Socket(
                   ip.AddressFamily,
@@ -34,80 +75,134 @@ namespace TCPGeckoAromaLibrary
 
             client.Connect(ip);
             connected = true;
+            commandHandler = new Thread(CommandHandler);
+            commandHandler.Start();
             return;
         }
 
         public void Disconnect()
         {
-            busy.Clear();
             client.Shutdown(SocketShutdown.Both);
             client.Disconnect(true);
             connected = false;
+            commandHandler.Join();
         }
 
         public string Peek(Datatype type, int address)
         {
+            if(address < 0x10000000)
+            {
+                return "Invalid";
+            }
             if (client == null)
             {
                 Console.WriteLine("Not Connected to wii u");
                 return "Error";
             }
-            busy.Add(address.ToString());
-
-            while (busy[0] != address.ToString())
-            {
-                //do nothing while address not on top of queue
-            }
 
             var message = "peek -t " + type + " -a " + $"0x{address:X}";
-            
-            if(debug) Console.WriteLine(message);
-            
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            client.Send(messageBytes);
+            var commandId = Guid.NewGuid().ToString();
 
-            string result = "";
-            do
+            commands.Enqueue((message, commandId));
+            results[commandId] = "";
+            if (debug) Console.WriteLine(message);
+            while (results[commandId] == "")
             {
-                int numberOfBytesReceived = client.Receive(messageBytes);
-                string t = Encoding.UTF8.GetString(messageBytes, 0, numberOfBytesReceived);
-                result += t;
-            } while (client.Available > 0);
 
-            busy.RemoveAt(0);
-            return result;
+            }
+            return results[commandId];
         }
 
         public void Poke(Datatype type, int address, int value)
         {
+            if (address < 0x10000000)
+            {
+                return;
+            }
             if (client == null)
             {
                 Console.WriteLine("Not Connected to wii u");
                 return;
             }
-            busy.Add(address.ToString());
-
-            while (busy[0] != address.ToString())
-            {
-                //do nothing while address not on top of queue
-            }
 
             var message = "poke -t " + type + " -a " + $"0x{address:X}" + " -v " + $"0x{value:X}";
+            var commandId = Guid.NewGuid().ToString();
 
+            commands.Enqueue((message, commandId));
+            results[commandId] = "";
             if (debug) Console.WriteLine(message);
-
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            client.Send(messageBytes);
-            string result = "";
-
-            do
+            while (results[commandId] == "")
             {
-                int numberOfBytesReceived = client.Receive(messageBytes);
-                string t = Encoding.UTF8.GetString(messageBytes, 0, numberOfBytesReceived);
-                result += t;
-            } while (client.Available > 0);
 
-            busy.RemoveAt(0);
+            }
+            return;
+        }
+
+        public List<string> PeekMultiple(Datatype type, int[] addresses)
+        {
+            foreach (int address in addresses)
+            {
+                if (address < 0x10000000)
+                {
+                    return new List<string>();
+                }
+            }
+
+            if (client == null)
+            {
+                Console.WriteLine("Not Connected to wii u");
+                return new List<string>();
+            }
+
+            var message = "peekmultiple -t " + type;
+            foreach (var addr in addresses)
+            {
+                message += " -a " + $"0x{addr:X}";
+            }
+            var commandId = Guid.NewGuid().ToString();
+
+            commands.Enqueue((message, commandId));
+            results[commandId] = "";
+            if (debug) Console.WriteLine(message);
+            while (results[commandId] == "")
+            {
+
+            }
+
+            return results[commandId].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        public void PokeMultiple(Datatype type, int[] addresses, int[] values)
+        {
+            foreach (int address in addresses)
+            {
+                if (address < 0x10000000)
+                {
+                    return;
+                }
+            }
+            if (client == null)
+            {
+                Console.WriteLine("Not Connected to wii u");
+                return;
+            }
+
+            var message = "pokemultiple -t " + type;
+
+            for(int i = 0; i < addresses.Length; i++)
+            {
+                message += " -a " + addresses[i] + " -v " + values[i];
+            }
+
+            var commandId = Guid.NewGuid().ToString();
+
+            commands.Enqueue((message, commandId));
+            results[commandId] = "";
+            if (debug) Console.WriteLine(message);
+            while (results[commandId] == "")
+            {
+
+            }
             return;
         }
 
@@ -118,33 +213,16 @@ namespace TCPGeckoAromaLibrary
                 Console.WriteLine("Not Connected to wii u");
                 return;
             }
-
-            string place = busy.Count.ToString();
-
-            busy.Add("Pause" + place);
-
-            while (busy[0] != "Pause" + place)
-            {
-                //do nothing while instruction not on top of queue
-            }
-            
-
             var message = "pause";
+            var commandId = Guid.NewGuid().ToString();
 
+            commands.Enqueue((message, commandId));
+            results[commandId] = "";
             if (debug) Console.WriteLine(message);
-
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            client.Send(messageBytes);
-            string result = "";
-
-            do
+            while (results[commandId] == "")
             {
-                int numberOfBytesReceived = client.Receive(messageBytes);
-                string t = Encoding.UTF8.GetString(messageBytes, 0, numberOfBytesReceived);
-                result += t;
-            } while (client.Available > 0);
 
-            busy.RemoveAt(0);
+            }
             return;
         }
 
@@ -155,32 +233,16 @@ namespace TCPGeckoAromaLibrary
                 Console.WriteLine("Not Connected to wii u");
                 return;
             }
-
-            string place = busy.Count.ToString();
-
-            busy.Add("Advance" + place);
-
-            while (busy[0] != "Advance" + place)
-            {
-                //do nothing while instruction not on top of queue
-            }
-
             var message = "advance";
+            var commandId = Guid.NewGuid().ToString();
 
+            commands.Enqueue((message, commandId));
+            results[commandId] = "";
             if (debug) Console.WriteLine(message);
-
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            client.Send(messageBytes);
-            string result = "";
-
-            do
+            while (results[commandId] == "")
             {
-                int numberOfBytesReceived = client.Receive(messageBytes);
-                string t = Encoding.UTF8.GetString(messageBytes, 0, numberOfBytesReceived);
-                result += t;
-            } while (client.Available > 0);
 
-            busy.RemoveAt(0);
+            }
             return;
         }
 
@@ -191,32 +253,16 @@ namespace TCPGeckoAromaLibrary
                 Console.WriteLine("Not Connected to wii u");
                 return;
             }
-
-            string place = busy.Count.ToString();
-
-            busy.Add("Resume" + place);
-
-            while (busy[0] != "Resume" + place)
-            {
-                //do nothing while instruction not on top of queue
-            }
-
             var message = "resume";
+            var commandId = Guid.NewGuid().ToString();
 
+            commands.Enqueue((message, commandId));
+            results[commandId] = "";
             if (debug) Console.WriteLine(message);
-
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            client.Send(messageBytes);
-            string result = "";
-
-            do
+            while (results[commandId] == "")
             {
-                int numberOfBytesReceived = client.Receive(messageBytes);
-                string t = Encoding.UTF8.GetString(messageBytes, 0, numberOfBytesReceived);
-                result += t;
-            } while (client.Available > 0);
 
-            busy.RemoveAt(0);
+            }
             return;
         }
 
